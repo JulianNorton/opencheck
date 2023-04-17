f91b87fd3bcc9ae9d280dbdb81e4dcb530d84b35
Julian Norton <julian@juliannorton.com>
2023-04-17 01:33:37 -0400

    working on spf record checker

--- spf-analyzer.js
+++ spf-analyzer.js
@@ -1,5 +1,6 @@
 const analyzeSpf = (txtRecords) => {
-    const spfRecords = txtRecords.filter(record => record.data && record.data.startsWith('v=spf1'));
+    const flattenedTxtRecords = txtRecords.flat();
+    const spfRecords = flattenedTxtRecords.filter((record) => record.startsWith('v=spf1'));
     const issues = [];
 
     if (spfRecords.length === 0) {
@@ -7,10 +8,42 @@ const analyzeSpf = (txtRecords) => {
     } else if (spfRecords.length > 1) {
         issues.push("Multiple SPF records found. It's recommended to have only one SPF record.");
     }
+    else {
+        const spfRecord = spfRecords[0] && spfRecords[0].data;
+        if (spfRecord) {
+            const checks = [
+                {
+                    condition: !spfRecord.includes('mx') && !spfRecord.includes('a'),
+                    message: "Neither 'a' nor 'mx' mechanisms found. At least one should be included to specify authorized senders."
+                },
+                {
+                    condition: spfRecord.includes('all'),
+                    message: "'all' mechanism found. This allows anyone to send mail from your domain."
+                },
+                {
+                    condition: spfRecord.includes('ptr'),
+                    message: "'ptr' mechanism found. Its use is discouraged due to performance and security concerns."
+                },
+                {
+                    condition: (spfRecord.match(/ip4:/g) || []).length > 2,
+                    message: "More than 2 'ip4:' mechanisms found. It's recommended to minimize the number of IP addresses allowed to send mail."
+                },
+                {
+                    condition: spfRecord.includes('?all'),
+                    message: "'?all' mechanism found. This is a neutral result and provides no authentication value."
+                },
+                {
+                    condition: spfRecord.includes('~all'),
+                    message: "'~all' mechanism found (softfail). Mail from unauthorized sources is accepted but marked as suspect. It's better to use '-all' (fail) for stronger enforcement."
+                },
+            ];
+        } else {
+            issues.push("Error reading SPF record data.");
+        }
+    }
 
     return issues;
 };
-
 
 module.exports = {
     analyzeSpf
